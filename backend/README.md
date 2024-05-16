fb62a5c273efa185502a7789a0db2368c463c424
Matei Tudose <matei@mateitudose.dev>
2024-05-17 00:42:56 +0300

    Add missing files

---
+++ backend/README.md
@@ -0,0 +1,15 @@
+# LangLearnBot - Frontend
+
+This is the proxy server used by the frontend to communicate with the Anthropic API, in order not to leak the API key.
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
---
+++ backend/index.ts
@@ -0,0 +1,18 @@
+import generateReadingTest from "./generateReadingTest.ts";
+
+Bun.serve({
+    async fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === "/generateReadingTest") {
+            const level = url.searchParams.get("level");
+            if (!level) {
+                return new Response("400: Missing level parameter", {status: 400});
+            }
+            return generateReadingTest(level)
+                .then((test) => new Response(JSON.stringify(test), {headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"}}))
+                .catch((error) => new Response("500: " + error.message, {status: 500}));
+        }
+        return new Response("404!", {status: 404});
+    },
+    port: 3000,
+});
